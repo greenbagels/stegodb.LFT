f21b3a60774aa77c6cc018b2f2d760c8a410007d
Sameed Pervaiz <pervaiz.8@osu.edu>
2022-01-13 20:37:33 -0500

    Fix key constraints in database creation
    Previously I wasn't aware that the MySQL constraints are globally named,
    so they need a different name in each table; they have been renamed with
    the format (pk/fk)_TABLENAME_KEY_NAME to fix this.

--- create_database.sql
+++ create_database.sql
@@ -5,53 +5,71 @@
 CREATE DATABASE ucr6;
 USE ucr6;
 
-CREATE TABLE map (
+CREATE TABLE maps (
     name VARCHAR(64) NOT NULL,
-    CONSTRAINT pk_name PRIMARY KEY(name)
+
+    PRIMARY KEY(name)
 );
 
-CREATE TABLE site (
+CREATE TABLE sites (
     map VARCHAR(64) NOT NULL,
     site1 VARCHAR(64) NOT NULL,
     site2 VARCHAR(64) NOT NULL,
-    CONSTRAINT pk_site PRIMARY KEY(map, site1, site2),
-    CONSTRAINT fk_map FOREIGN KEY(map) REFERENCES map(name)
+
+    PRIMARY KEY(map, site1, site2),
+    CONSTRAINT fk_site_map FOREIGN KEY(map) REFERENCES maps(name)
 );
 
-CREATE TABLE team (
+CREATE TABLE teams (
     name VARCHAR(64) NOT NULL,
-    CONSTRAINT pk_team PRIMARY KEY(name)
+    -- attack_wins INT NOT NULL,
+    -- defense_wins INT NOT NULL,
+    -- round wins derived from these two above
+    -- game_wins INT NOT NULL,
+    -- match_wins INT NOT NULL,
+    PRIMARY KEY(name)
 );
 
-CREATE TABLE player (
+CREATE TABLE players (
     -- uuid is the r6 tracker uuid: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
     uuid CHAR(36) NOT NULL,
     given_name VARCHAR(255) NOT NULL,
     family_name VARCHAR(255) NOT NULL,
-    team_name VARCHAR(64) NOT NULL,
-    CONSTRAINT pk_player PRIMARY KEY(uuid),
-    CONSTRAINT fk_team_name FOREIGN KEY(team_name) REFERENCES team(name)
+    username VARCHAR(255) NOT NULL,
+    team VARCHAR(64) NOT NULL,
+
+    PRIMARY KEY(uuid),
+    CONSTRAINT fk_player_team FOREIGN KEY(team) REFERENCES teams(name)
 );
 
-create TABLE game_series (
-    id VARCHAR(64) NOT NULL,
-    date_ DATE NOT NULL,
+create TABLE matches (
+    match_date DATE NOT NULL,
+    team VARCHAR(64) NOT NULL,
     opponent VARCHAR(64) NOT NULL,
-    CONSTRAINT pk_match PRIMARY KEY(id)
+
+    PRIMARY KEY(match_date, team, opponent),
+    CONSTRAINT fk_match_team FOREIGN KEY(team) REFERENCES teams(name)
 );
 
-create TABLE game (
+create TABLE games (
     id VARCHAR(64) NOT NULL,
     map VARCHAR(64) NOT NULL,
-    -- TODO: figure out a good way to id game series...
-    series_id VARCHAR(64) NOT NULL,
+
+    -- foreign composite primary key >.>
+    match_date DATE NOT NULL,
+    match_team VARCHAR(64) NOT NULL,
+    match_opp VARCHAR(64) NOT NULL,
+
     num_rounds INT NOT NULL,
-    CONSTRAINT pk_id PRIMARY KEY(id),
-    CONSTRAINT fk_series_id FOREIGN KEY(series_id) REFERENCES game_series(id),
-    CONSTRAINT fk_map_name FOREIGN KEY(map) REFERENCES map(name)
+    -- todo: how to handle checking if winners are valid teams...?
+
+    PRIMARY KEY(id),
+    CONSTRAINT fk_game_match FOREIGN KEY(match_date, match_team, match_opp)
+        REFERENCES matches(match_date, team, opponent),
+    CONSTRAINT fk_game_map FOREIGN KEY(map) REFERENCES maps(name)
 );
 
-create TABLE performance (
+create TABLE performances (
     player_uuid CHAR(36) NOT NULL,
     game_id VARCHAR(64) NOT NULL,
     -- Counter statistics:
@@ -84,7 +102,7 @@ create TABLE performance (
     -- srv can be calculated from game(id).rounds - deaths
     -- hs ratio can be calculated from headshots / kills
 
-    CONSTRAINT pk_performance PRIMARY KEY(player_uuid, game_id),
-    CONSTRAINT fk_player_uuid FOREIGN KEY(player_uuid) REFERENCES player(uuid),
-    CONSTRAINT fk_game_id FOREIGN KEY(game_id) REFERENCES game(id)
+    PRIMARY KEY(player_uuid, game_id),
+    CONSTRAINT fk_perf_player FOREIGN KEY(player_uuid) REFERENCES players(uuid),
+    CONSTRAINT fk_perf_game FOREIGN KEY(game_id) REFERENCES games(id)
 );
